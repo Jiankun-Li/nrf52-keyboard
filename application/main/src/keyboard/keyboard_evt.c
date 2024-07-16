236eb71c295dfb2d75c40237cd6173297d8264a4
Jiankun-Li <38851666+Jiankun-Li@users.noreply.github.com>
2024-07-16 10:17:02 +0800

    Update keyboard_evt.c

--- application/main/src/keyboard/keyboard_evt.c
+++ application/main/src/keyboard/keyboard_evt.c
@@ -125,12 +125,13 @@ static void internal_event_handler(enum user_event event, void* arg)
         break;
     case USER_EVT_BLE_STATE_CHANGE:
         // 蓝牙没有连接，若没有接通电源则过1s睡眠
-        if (subEvent == BLE_STATE_DISCONNECT) {
-            if (power_attached)
+        if (subEvent == BLE_STATE_IDLE) {
+            if (power_attached){
                advertising_slow();
-            else
+            }else{
                 wait_ms(1000);
                 sleep(SLEEP_NO_CONNECTION);
+            }
         }
         break;
     case USER_EVT_BLE_PASSKEY_STATE:
